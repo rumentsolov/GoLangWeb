147a9050d886595a3d2ff89240cdf75e320badc0
Rumen Tsolov <r.tsolov86@gmail.com>
2023-02-20 20:56:21 +0200

    templates added

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,18 +1,19 @@
 package handlers
 
 import (
-	"github.com/rumentsolov/GoLangWeb/render"
 	"net/http"
+
+	"github.com/rumentsolov/GoLangWeb/render"
 )
 
 // ? Home is the about page handler
 func Home(w http.ResponseWriter, r *http.Request) {
 
-	render.RenderTemplalte(w, "home.page.html") 
+	render.RenderTemplalte(w, "home.page.tmpl")
 }
 
 // ? About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 
-	render.RenderTemplalte(w, "about.page.html")
+	render.RenderTemplalte(w, "about.page.tmpl")
 }
--- render/render.go
+++ render/render.go
@@ -7,9 +7,9 @@ import (
 	"text/template"
 )
 
-//? Renders the templates using html/template
-func RenderTemplalte(w http.ResponseWriter, html5 string) {
-	parsedTemplate, _ := template.ParseFiles("./html/" + html5)
+// ? Renders the templates using html/template
+func RenderTemplalte(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Printf("Error parsing template %s \n", err)
